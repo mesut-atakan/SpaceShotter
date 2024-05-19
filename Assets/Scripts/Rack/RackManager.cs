be3e295de2d33e63132148fcf0f10f1252635811
mesut_atakan <mesut_atakan5@hotmail.com>
2024-05-19 17:33:16 +0300

    Rack Manager Update

--- Assets/Scripts/Rack/RackManager.cs
+++ Assets/Scripts/Rack/RackManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -13,6 +15,7 @@ public class RackManager : MonoBehaviour
     [SerializeField, Range(0, 5)] private float asteroidSpeed = 3;
     [SerializeField] private byte spawnMaxObject = 4;
     [SerializeField] private Vector2 spawnObjectClamp;
+    [SerializeField] private float zAxis;
 
 
     [Header("Object Pool Properties")]
@@ -26,6 +29,8 @@ public class RackManager : MonoBehaviour
     #region <<<< Private Fields >>>>
 
     private ObjectPooling<Rack> objectPooling;
+    private bool _isSpawner = true;
+    [SerializeField] private List<Rack> _activeRacks = new List<Rack>();
 
     #endregion <<<< XXX >>>>
 
@@ -51,4 +56,114 @@ public class RackManager : MonoBehaviour
     }
 
 
+    private void Start()
+    {
+        StartCoroutine(SpawnerCalculator());
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(new Vector3(-100, 0, this.zAxis), new Vector3(100, 0, zAxis));
+    }
+
+
+
+
+
+
+    private IEnumerator SpawnerCalculator()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(this.spawnDuration);
+            Spawner();
+
+        }
+    }
+
+
+
+
+    private void Spawner()
+    {
+        // ~~ Variables ~~
+        Rack _createRack;
+
+        for (int i = 0; i < this.spawnMaxObject; i++)
+        {
+            _createRack = this.objectPooling.Get();
+            AddActiveRack(_createRack);
+            _createRack.transform.position = GetPosition();
+        }
+    }
+
+
+
+    /// <summary>
+    /// Bu fonksiyon ile Active Rack listesi icerisine `Rack` sinifi ekleyebilirsiniz
+    /// </summary>
+    /// <param name="rack">Eklemek istediginiz rack sinifini parametre olarak giriniz!</param>
+    private void AddActiveRack(Rack rack)
+    {
+        if (!this._activeRacks.Contains(rack))
+        {
+            this._activeRacks.Add(rack);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    /// <summary>
+    /// Bu fonksiyon ile birlikte asteroid taslarinin birbirinden uzak bir sekilde olusmasini saglayabilirsiniz!
+    /// </summary>
+    /// <returns>Uygun pozisyon geri donderilecektir!</returns>
+    private Vector3 GetPosition()
+    {
+        // ~~ Variables ~~
+        Vector3 _pos;
+        float _distance;
+        bool _isPos = true;
+        int _testValue = 0;
+
+
+        _pos = Vector3.zero;
+
+        _pos.z = this.zAxis;
+        do
+        {
+            _testValue++;
+            _isPos = true;
+
+            _pos.x = Random.Range(this.spawnObjectClamp.x, this.spawnObjectClamp.y);
+
+            foreach (Rack _rack in this._activeRacks)
+            {
+                _distance = Mathf.Abs(_rack.transform.position.x - _pos.x);
+
+                if (_distance < 4)
+                {
+                    _isPos = false;
+                    break;
+                }
+            }
+            if (_isPos)
+                return _pos;
+        } while (_testValue < 10000);
+
+        Debug.LogError("<color=red>Error</color> Uygun Konum bulunamadi!", this.gameObject);
+        return Vector3.zero;
+    }
+    
 }
