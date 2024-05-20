9799a582907ae648e88b567d930456f363d7157b
mesut_atakan <mesut_atakan5@hotmail.com>
2024-05-20 21:44:07 +0300

    Collision Controller Change Name

--- Assets/Scripts/Player/PlayerCollisionController.cs
+++ Assets/Scripts/Player/PlayerCollisionController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 
 
-public class CollisionController : MonoBehaviour, IInteraction
+public class PlayerCollisionController : MonoBehaviour, IInteraction
 {
     public void Damage(Collider other)
     {
